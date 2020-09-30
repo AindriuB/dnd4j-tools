4a62a496979bdd61476c6eea2d0f8b15bff47294
AindriuB <aindriubannister@gmail.com>
2020-09-30 20:57:55 +0100

    Update parsing weapons, spells, armour, items

--- src/main/java/ie/dnd4j/loader/aurora/WebCompendiumLoader.java
+++ src/main/java/ie/dnd4j/loader/aurora/WebCompendiumLoader.java
@@ -3,7 +3,6 @@ package ie.dnd4j.loader.aurora;
 import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,15 +18,17 @@ import org.springframework.util.xml.DomUtils;
 import org.springframework.web.client.RestTemplate;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import ie.dnd4j.Compendium;
 import ie.dnd4j.configuration.CompendiumConfiguration;
 import ie.dnd4j.configuration.CompendiumSourcesConfiguration;
+import ie.dnd4j.items.Armour;
 import ie.dnd4j.items.Tools;
+import ie.dnd4j.items.Weapon;
 import ie.dnd4j.loader.CompendiumLoader;
+import ie.dnd4j.spells.Spell;
 
 public class WebCompendiumLoader implements CompendiumLoader {
 
@@ -41,6 +42,7 @@ public class WebCompendiumLoader implements CompendiumLoader {
 
     public WebCompendiumLoader() {
 	compendium = new Compendium();
+	compendium.setSource("Aurora");
     }
 
     /***
@@ -75,7 +77,6 @@ public class WebCompendiumLoader implements CompendiumLoader {
 	    });
 	    buildCompendium(documentMap);
 	}
-	
 
     }
 
@@ -108,83 +109,106 @@ public class WebCompendiumLoader implements CompendiumLoader {
     }
 
     private void buildCompendium(Map<String, Document> documents) {
-	if(documents.isEmpty()) {
+	if (documents.isEmpty()) {
 	    return;
 	}
-	
+
 	documents.entrySet().stream().forEach(entrySet -> {
-	    LOGGER.info("Building compendium from document {}", entrySet.getKey() );
+	    LOGGER.info("Building compendium from document {}", entrySet.getKey());
 	    Document document = entrySet.getValue();
-	    NodeList list =  document.getElementsByTagName("element");
-	    
-	    for(int i = 0; i < list.getLength(); i++) {
-		addNode((Element) list.item(i));
+	    NodeList list = document.getElementsByTagName("element");
+
+	    for (int i = 0; i < list.getLength(); i++) {
+		processNode((Element) list.item(i));
 	    }
-	    LOGGER.info("Elements: {}", list.getLength());	 
+	    LOGGER.info("Elements: {}", list.getLength());
 	});
     }
 
-    
-    private void addNode(Element  element) {
-	String type = element.getAttributes().getNamedItem("type").getNodeValue();
-	
-	LOGGER.debug("Processing node of type: {}", type);
-	
-	switch(type) {
-	case "Item":
-	    processItemNode(element);
-	    break;
-	default:
-	    break;
-	}
-	
-    }
-    
-    
-    private void processItemNode(Element  element) {
+    private void processNode(Element element) {
 
 	Map<String, String> settersMap = new HashMap<String, String>();
-	
+
 	String name = element.getAttribute("name");
-	String type = element.getAttribute("type");	
+	String type = element.getAttribute("type");
 	String source = element.getAttribute("source");
 	String id = element.getAttribute("id");
-	
-	
-	
+
 	List<Element> setters = DomUtils.getChildElementsByTagName(element, "setters");
-	for(Element e : setters) {
-	   List<Element> sets = DomUtils.getChildElementsByTagName(e, "set");
-	   sets.stream().forEach( node -> {
-	       settersMap.put(node.getAttribute("name").toLowerCase() , node.getTextContent();
-	   });		
+	for (Element e : setters) {
+	    List<Element> sets = DomUtils.getChildElementsByTagName(e, "set");
+	    sets.stream().forEach(node -> {
+		settersMap.put(node.getAttribute("name").toLowerCase(), node.getTextContent());
+		String currency = node.getAttribute("currency").toLowerCase();
+		if (currency != null && !currency.isEmpty()) {
+		    settersMap.put("currency", currency);
+		}
+	    });
 	}
-	
-	if(settersMap.containsKey("category")) {
-	    
-	    String category = settersMap.get("category");
-	    switch(category.toLowerCase()) {
-	    case "tools":
-		Tools tools = new Tools();
-		tools.setCost(Integer.valueOf(settersMap.get("cost")));
-		tools.setName(name);
-		tools.setProficiency(settersMap.get("proficiency"));
-		tools.setSource(source);
-		tools.setType(type);
-		tools.tag(id);
-		this.compendium.getItems().put(id, tools);
-		break;
-		default:
-	    
-	    }
-	    
+
+	switch (type.toLowerCase()) {
+	case "item":
+	    Tools tools = new Tools();
+	    tools.setCost(Integer.valueOf(settersMap.get("cost")));
+	    tools.setName(name);
+	    tools.setProficiency(settersMap.get("proficiency"));
+	    tools.setSource(source);
+	    tools.setWeight(settersMap.get("weight"));
+	    tools.setCurrency(settersMap.get("currency"));
+	    tools.setCategory(settersMap.get("category"));
+	    tools.setType(type);
+	    tools.tag(id);
+	    this.compendium.getItems().put(id, tools);
+	    break;
+	case "armor":
+	    Armour armour = new Armour();
+	    armour.setCost(Integer.valueOf(settersMap.get("cost")));
+	    armour.setName(name);
+	    armour.setProficiency(settersMap.get("proficiency"));
+	    armour.setSource(source);
+	    armour.setWeight(settersMap.get("weight"));
+	    armour.setCurrency(settersMap.get("currency"));
+	    armour.setCategory(settersMap.get("category"));
+	    armour.setType(type);
+	    armour.tag(id);
+	    this.compendium.getItems().put(id, armour);
+	    break;
+	case "weapon":
+	    Weapon weapon = new Weapon();
+	    weapon.setCost(Integer.valueOf(settersMap.get("cost")));
+	    weapon.setName(name);
+	    weapon.setProficiency(settersMap.get("proficiency"));
+	    weapon.setSource(source);
+	    weapon.setWeight(settersMap.get("weight"));
+	    weapon.setCurrency(settersMap.get("currency"));
+	    weapon.setCategory(settersMap.get("category"));
+	    weapon.setType(type);
+	    weapon.tag(id);
+	    this.compendium.getItems().put(id, weapon);
+	    break;
+	case "spell":
+	    Spell spell = new Spell();
+	    spell.setName(name);
+	    spell.setCastTime(settersMap.get("time"));
+	    spell.setClasses("");
+	    spell.setConcentration(Boolean.valueOf(settersMap.get("isConcentration")));
+	    spell.setDuration(settersMap.get("duration"));
+	    spell.setLevel(Integer.parseInt(settersMap.get("level")));
+	    spell.setMaterialComponens(settersMap.get("materialComponent"));
+	    spell.setMaterialComponent(Boolean.valueOf(settersMap.get("hasMaterialComponent")));
+	    spell.setRange(settersMap.get("range"));
+	    spell.setRitual(Boolean.valueOf(settersMap.get("isRitual")));
+	    spell.setSchool(settersMap.get("school"));
+	    spell.setSomaticComponent(Boolean.valueOf(settersMap.get("hasSomaticComponent")));
+	    spell.setVerbalComponent(Boolean.valueOf("hasVerbalComponent"));
+	    spell.tag(id);
+	    this.compendium.getSpells().put(id, spell);
+
+	default:
 	}
-	
-	
-	
+
     }
-    
-    
+
     public CompendiumSourcesConfiguration getCompendiumConfiguration() {
 	return compendiumConfiguration;
     }
